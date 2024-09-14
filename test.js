8983f482bb96066b0c63ac68d7b4df7c2c4a4b05
omprakash11code <124289672+omprakash11code@users.noreply.github.com>
2024-09-14 14:41:30 +0000

    Dates concept

---
+++ test.js
@@ -0,0 +1,7 @@
+let dinaak=new Date();
+console.log(dinaak);
+console.log(dinaak.toDateString());
+console.log(dinaak.toLocaleDateString);
+console.log(typeof dinaak);
+let myDate= new Date(2023,0,23,5,3);
+console.log(myDate);
