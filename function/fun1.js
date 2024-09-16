67d4136aaa4cc64f6a441911ededde2c70b624ea
omprakash11code <124289672+omprakash11code@users.noreply.github.com>
2024-09-16 15:32:10 +0000

    fun basic

---
+++ function/fun1.js
@@ -0,0 +1,13 @@
+// function addtwonum(num1,num2){
+//     console.log(num1+num2)
+// }
+// addtwonum(10,10)
+// const res=addtwonum(10,10);
+//console.log(res);//o/p-undefined
+
+function addtwonum(num1,num2){
+  let res=num1+num2
+  return res;
+}
+const res=addtwonum(3,7)
+console.log(res);//o/p-undefined
